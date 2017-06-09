93d631ab6e224959e73f38943c3d53083291039c
AnnSamoluk <annsamoluk@mail.ru>
2017-06-09 12:25:39 +0300

    Add entity

---
+++ DAL/Entity/MyContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entity
+{
+    public class MyContext: DbContext
+    {
+        public MyContext() : base("MyDbConnection")
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
