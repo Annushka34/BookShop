d4dfc0c28f0b3bfbf0f42043338e0de2fb3ec5a9
Zergond-ПК\Zergond <azimut965@gmail.com>
2017-06-15 03:10:44 +0300

    Fill functional for UserRepository

--- DAL/ConcreteRepositories/UserRepository.cs
+++ DAL/ConcreteRepositories/UserRepository.cs
@@ -14,12 +14,19 @@ namespace DAL.ConcreteRepositories
 
         public bool CheckUserLogin(string login)
         {
-            throw new NotImplementedException();
+            db = new MyContext();
+            var user = db.Users.Where(x => x.Login == login).FirstOrDefault();
+            if (user != null)
+            {
+                return true;
+            }
+            else
+                throw new NotImplementedException("User by this login not exists");
         }
 
         public bool Create(User user)
         {
-            db=new MyContext();
+            db = new MyContext();
             db.Users.Add(user);
             db.SaveChanges();
             return true;
@@ -27,22 +34,51 @@ namespace DAL.ConcreteRepositories
 
         public bool Delete(User user)
         {
-            throw new NotImplementedException();
+            db = new MyContext();
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return true;
         }
 
         public User Read(int userId)
         {
-            throw new NotImplementedException();
+            db = new MyContext();
+            User user = db.Users.Find(userId);
+            if (user != null)
+            {
+                return user;
+            }
+            else
+                throw new NotImplementedException("Cannot read user");
         }
 
         public User ReadByLogin(string login)
         {
-            throw new NotImplementedException();
+            db = new MyContext();
+            var user = db.Users.Where(x => x.Login == login).FirstOrDefault();
+            if (user != null)
+            {
+                return user;
+            }
+            else
+                throw new NotImplementedException("User by login not found");
         }
 
         public bool Update(User userOld, User userNew)
         {
-            throw new NotImplementedException();
+            db = new MyContext();
+            var user = db.Users.Find(userOld.Id);
+            if (user != null)
+            {
+                user.Login = userNew.Login;
+                user.Password = userNew.Password;
+                user.Email = userNew.Email;
+                user.Admin = userNew.Admin;
+                user.Customer = userNew.Customer;
+                db.SaveChanges();
+                return true;
+            }
+            throw new NotImplementedException("User cannot update");
         }
     }
 }
